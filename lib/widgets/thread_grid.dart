04bebd2b307d679633aa38795a76cd11d0a0e808
Sophia (Turner) <me@sophia-dev.io>
2025-05-24 23:33:18 +0900

    feat(threads): make thread grid item transparent when its read

--- lib/widgets/thread_grid.dart
+++ lib/widgets/thread_grid.dart
@@ -1,3 +1,4 @@
+import 'package:cabinet/entities/thread_read_status.dart';
 import 'package:cabinet/queries/watcherThreads.graphql.dart';
 import 'package:cabinet/widgets/thread_grid_item.dart';
 import 'package:flutter/material.dart';
@@ -5,8 +6,14 @@ import 'package:flutter/material.dart';
 class ThreadGrid extends StatefulWidget {
   final Function(Fragment$MinimalThread)? onThreadTap;
   final List<Fragment$MinimalThread> threads;
+  final List<ThreadReadStatus>? readStatus;
 
-  const ThreadGrid({super.key, this.onThreadTap, required this.threads});
+  const ThreadGrid({
+    super.key,
+    this.onThreadTap,
+    required this.threads,
+    this.readStatus,
+  });
 
   @override
   State<ThreadGrid> createState() => _ThreadGridState();
@@ -16,6 +23,7 @@ class _ThreadGridState extends State<ThreadGrid> {
   @override
   Widget build(BuildContext context) {
     final threads = widget.threads;
+    final readStatus = widget.readStatus ?? [];
 
     return GridView(
       padding: EdgeInsets.zero,
@@ -25,15 +33,15 @@ class _ThreadGridState extends State<ThreadGrid> {
       ),
       children: List.generate(
         threads.length,
-        (index) => ThreadGridItem(
-          thread: threads[index],
-          onTap: (thread) {
-            if (widget.onThreadTap == null) {
-              return;
-            }
-
-            widget.onThreadTap!(thread);
-          },
+        (index) => Opacity(
+          opacity:
+              readStatus.any((status) => status.threadId == threads[index].id)
+                  ? 0.5
+                  : 1.0,
+          child: ThreadGridItem(
+            thread: threads[index],
+            onTap: widget.onThreadTap,
+          ),
         ),
       ),
     );
